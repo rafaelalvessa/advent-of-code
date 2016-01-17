25de9a994df9ba329f3fab8d9d8feea6a574a6b8
Raph Alves Sa <raph.alvessa@gmail.com>
2016-01-17 02:55:58 +0000

    add solution for part one and two of day 1

---
+++ day1/day1.dart
@@ -0,0 +1,63 @@
+import 'dart:io' show File;
+
+/// Shows the solution for part one and two of the problem.
+void main() {
+  var input;
+  try {
+    input = new File('input').readAsStringSync();
+    print('Part one: ${partOne(input)}');
+    var basement = partTwo(input);
+    print('Part two: ${basement ?? 'Santa never entered the basement.'}');
+  } on FileSystemException catch (e) {
+    print('Error reading input from file: $e');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+
+/// Returns the number of the floor Santa should go to after following each
+/// instruction in [instructions].
+///
+/// Throws an [Exception] if an invalid instruction is found.
+int partOne(String instructions) {
+  var floor = 0;
+  for (var i = 0; i < instructions.length; i++) {
+    switch (instructions[i]) {
+      case '(':
+        floor++;
+        break;
+      case ')':
+        floor--;
+        break;
+      case '\n':
+        break;
+      default:
+        throw 'Invalid instruction at index $i: \'${instructions[i]}\'.';
+    }
+  }
+  return floor;
+}
+
+/// Returns the number of the instruction in [instructions] that causes Santa to
+/// first enter the basement, or [null] if Santa never enters the basement.
+///
+/// Throws an [Exception] if an invalid instruction is found.
+int partTwo(String instructions) {
+  var floor = 0;
+  for (var i = 0; i < instructions.length; i++) {
+    switch (instructions[i]) {
+      case '(':
+        floor++;
+        break;
+      case ')':
+        floor--;
+        break;
+      case '\n':
+        break;
+      default:
+        throw 'Invalid instruction at index $i: \'${instructions[i]}\'.';
+    }
+    if (floor == -1) return i + 1;
+  }
+  return null;
+}

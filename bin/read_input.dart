6ceff70c90260bdd8cb883994fb0560aa0f2f429
Raph Alves Sa <raph.alvessa@gmail.com>
2016-04-09 00:14:49 +0000

    Separate input reading for reusability

---
+++ bin/read_input.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+/// Returns a [String] with the contents of the file named [filename].
+///
+/// An error message is shown instead if the operation fails.
+String readInput(String filename) {
+  try {
+    return new File(filename).readAsStringSync();
+  } on FileSystemException catch (e) {
+    print('Error reading input from file: $e');
+  }
+}

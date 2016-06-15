0847d7d27cb0e317e85fd8f44ce83c9845e01e8b
Raph Alves Sa <raph.alvessa@gmail.com>
2016-06-15 22:43:26 +0100

    Add solution to day 5 part 2

--- bin/day5.dart
+++ bin/day5.dart
@@ -7,6 +7,7 @@ void main() {
     var input = readInput('day5');
     var strings = input.split('\n');
     print('Part one: ${partOne(strings)}');
+    print('Part two: ${partTwo(strings)}');
   } on FileSystemException catch (e) {
     print('Error reading input from file: $e');
   } catch (e) {
@@ -14,17 +15,26 @@ void main() {
   }
 }
 
-/// Returns the number of nice strings in [strings].
+/// Returns the number of nice strings in [strings] according to model one.
 int partOne(List<String> strings) {
   var niceStrings = 0;
   for (var i = 0; i < strings.length; i++) {
-    if (_isNice(strings[i])) niceStrings++;
+    if (_isNiceModelOne(strings[i])) niceStrings++;
   }
   return niceStrings;
 }
 
-/// Returns `true` if [string] is a nice string.
-bool _isNice(String string) =>
+/// Returns the number of nice strings in [strings] according to model two.
+int partTwo(List<String> strings) {
+  var niceStrings = 0;
+  for (var i = 0; i < strings.length; i++) {
+    if (_isNiceModelTwo(strings[i])) niceStrings++;
+  }
+  return niceStrings;
+}
+
+/// Returns `true` if [string] is a nice string according to model one.
+bool _isNiceModelOne(String string) =>
     _hasThreeVowels(string) && _hasLetterTwice(string) &&
     !_hasForbiddenString(string);
 
@@ -33,9 +43,23 @@ bool _hasThreeVowels(String string) =>
     new RegExp(r'[aeiou]').allMatches(string).length >= 3;
 
 /// Returns `true` if [string] contains one letter that appears at least twice.
-bool _hasLetterTwice(String string) => string.contains(new RegExp(r'(.)\1'));
+bool _hasLetterTwice(String string) => string.contains(new RegExp(r'(\w)\1'));
 
 /// Returns `true` if [string] contains a forbidden string: "ab", "cd", "pq" or
 /// "xy".
 bool _hasForbiddenString(String string) =>
     string.contains(new RegExp(r'ab|cd|pq|xy'));
+
+/// Returns `true` if [string] is a nice string according to model two.
+bool _isNiceModelTwo(String string) =>
+    _hasTwoLetterPairTwice(string) && _hasRepeatedLetterSeparated(string);
+
+/// Returns `true` if [string] contains a pair of any two letters that appears
+/// at least twice without overlapping.
+bool _hasTwoLetterPairTwice(String string) =>
+    string.contains(new RegExp(r'(\w{2})\w*\1'));
+
+/// Returns `true` if [string] contains at least one letter which repeats with
+/// exactly one letter between them.
+bool _hasRepeatedLetterSeparated(String string) =>
+    string.contains(new RegExp(r'(\w)\w\1'));

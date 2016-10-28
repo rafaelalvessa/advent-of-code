40eaf6097666b4990242561cda21aa5fbf373d6d
Raph Alves Sa <raph.alvessa@gmail.com>
2016-10-28 01:20:12 +0100

    Add solution to day 6 part 2

--- bin/day6.dart
+++ bin/day6.dart
@@ -7,7 +7,7 @@ void main() {
     var input = readInput('day6');
     var strings = input.split('\n');
     print('Part one: ${partOne(strings)}');
-    // print('Part two: ${partTwo(strings)}');
+    print('Part two: ${partTwo(strings)}');
   } on FileSystemException catch (e) {
     print('Error reading input from file: $e');
   } catch (e) {
@@ -17,14 +17,37 @@ void main() {
 
 /// Returns the number of lights that are lit after following all the
 /// [instructions].
+///
+/// Throws an [Exception] if any of the instructions is invalid.
 int partOne(List<String> instructions) {
   var lights = new List<List<bool>>.generate(1000, (_) =>
       new List<bool>.filled(1000, false));
+
+  return _followInstructions(instructions, lights, _doTaskOne);
+}
+
+/// Returns the total brightness of all lights after following all the
+/// [instructions].
+///
+/// Throws an [Exception] if any of the instructions is invalid.
+int partTwo(List<String> instructions) {
+  var lights = new List<List<int>>.generate(1000, (_) =>
+      new List<int>.filled(1000, 0));
+  return _followInstructions(instructions, lights, _doTaskTwo);
+}
+
+/// Abstract function to return the result after following the [instructions]
+/// to change the [lights] by calling [doTask].
+///
+/// Throws an [Exception] if any of the instructions is invalid.
+int _followInstructions(List<String> instructions, List<List> lights,
+    Function doTask) {
   var task;
   var startX, startY;
   var endX, endY;
   var match;
-  var lit = 0;
+  var result = 0;
+  var change;
 
   RegExp exp = new RegExp(r'(turn \w+|toggle) (\d+),(\d+) through (\d+),(\d+)');
 
@@ -42,23 +65,55 @@ int partOne(List<String> instructions) {
 
     for (var x = startX; x <= endX; x++) {
       for (var y = startY; y <= endY; y++) {
-        switch (task) {
-          case 'turn on':
-            if (!lights[x][y]) lit++;
-            lights[x][y] = true;
-            break;
-          case 'turn off':
-            if (lights[x][y] && lit > 0) lit--;
-            lights[x][y] = false;
-            break;
-          case 'toggle':
-            lit += lights[x][y] ? (lit > 0 ? -1 : 0) : 1;
-            lights[x][y] = !lights[x][y];
-            break;
-        }
+        change = doTask(task, lights, x, y);
+        result += result == 0 && change < 0 ? 0 : change;
       }
     }
   }
 
-  return lit;
+  return result;
+}
+
+/// Returns the result of executing the [task] to change the light in [lights]
+/// at row [x] and column [y].
+int _doTaskOne(String task, List<List<bool>> lights, int x, int y) {
+  var result = 0;
+
+  switch (task) {
+    case 'turn on':
+      if (!lights[x][y]) result++;
+      lights[x][y] = true;
+      break;
+    case 'turn off':
+      if (lights[x][y]) result--;
+      lights[x][y] = false;
+      break;
+    case 'toggle':
+      result += lights[x][y] ? -1 : 1;
+      lights[x][y] = !lights[x][y];
+      break;
+  }
+
+  return result;
+}
+
+/// Returns the result of executing the [task] to change the light in [lights]
+/// at row [x] and column [y].
+int _doTaskTwo(String task, List<List<int>> lights, int x, int y) {
+  switch (task) {
+    case 'turn on':
+      lights[x][y]++;
+      return 1;
+    case 'turn off':
+      if (lights[x][y] > 0) {
+        lights[x][y]--;
+        return -1;
+      }
+      break;
+    case 'toggle':
+      lights[x][y] += 2;
+      return 2;
+  }
+
+  return 0;
 }
